e013bedffdc4c972ccaddced4813183171020a6c
pchvykov <pchvykov@gmail.com>
2019-10-26 15:16:12 -0500

    made a loop for iterating through tolopogies in Billy_edit (yes, tolopogies)

--- data_analyze.m
+++ data_analyze.m
@@ -1,5 +1,5 @@
 
-nAg=900; maxId=7;
+% nAg=900; maxId=7;
 effAll=zeros(size(idConnAll,3),size(idConnAll,4));
 for it=1:size(idConnAll,3) %time loop
   for ir=1:size(idConnAll,4) %random realizations loop
@@ -13,17 +13,17 @@ end
 effAllBck=effAll;
 %%
 effAll=log(movmean(effAllBck,1));
-clf; hold on; plN=15; dt=1;
+clf; hold on; plN=1; dt=1;
 plot(effAll(1:dt:end,1:plN),'r') %pref-att
 %
-plot(effAll(1:dt:end,15+(1:plN)),'b') %square
+plot(effAll(1:dt:end,rng_max+(1:plN)),'b') %square
 %
-plot(effAll(1:dt:end,30+(1:plN)),'g') %small world
+plot(effAll(1:dt:end,rng_max*2+(1:plN)),'g') %small world
 
 %%
 figure;
 idRem=squeeze(sum(sum(idConnAll)>0)); %number of remaining ideas
-clf; hold on; plN=15; dt=1;
+clf; hold on; plN=1; dt=1;
 plot(idRem(1:dt:end,1:plN),'r') %pref-att
-plot(idRem(1:dt:end,15+(1:plN)),'b') %square
-plot(idRem(1:dt:end,30+(1:plN)),'g') %small world
+plot(idRem(1:dt:end,1+(1:plN)),'b') %square
+plot(idRem(1:dt:end,2+(1:plN)),'g') %small world
--- dual_net_billy_edit.m
+++ dual_net_billy_edit.m
@@ -16,12 +16,14 @@
 
 clear;
 clf;
-nAgSqrt=30;
+nAgSqrt=20;
 nAg=nAgSqrt^2; nId=40; %number of agents and ideas
-maxId=7; tSteps=1E7;
+maxId=7; tSteps=1E6;
 socConn=spalloc(nAg,nAg,2*nAg);
 agreeFl=true; saveMov=false;
-rng_max = 15;
+rng_max = 1; %number of random realizations
+idConnAll=[]; %data storage
+for net_type=1:3
 
 for rng_seed=1:rng_max
 
@@ -29,11 +31,13 @@ for rng_seed=1:rng_max
     %% Construct the network
     ndDeg=zeros(1,nAg)+1E-3;
 %     rng_seed = 4;
-    rng(rng_seed);
+    rng(100+rng_seed);
 
+switch net_type
+  case 1
     % % ----1---- BA (preferential attachment)
     for ii=1:nAg
-      for ir=1:20; rand; end %ensure rand is uncorrelated
+      for ir=1:20; rand; end %ensure rand is uncorrelated <-crazy that this matters...
       for ie=1
         if(ie>1  && rand>1.01); continue; end %modulate connectivity
       attTo=randsample(nAg,1,true,ndDeg.^1); %preferntial attachment
@@ -41,294 +45,18 @@ for rng_seed=1:rng_max
       ndDeg(ii)=round(ndDeg(ii)+1); ndDeg(attTo)=round(ndDeg(attTo)+1);
       end
     end
-
-    % ----2---- Square Lattice
-%     socConn=delsq(numgrid('S', nAgSqrt+2)); socConn=-socConn+diag(diag(socConn));
-
-    % % ----3---- WS
-    % % Watts-Strogatz model graph with N
-    % % nodes, N*K edges, mean node degree 2*K, and rewiring probability beta.
-    % %
-    % % beta = 0 is a ring lattice, and beta = 1 is a random graph.
-    % 
-    % % Connect each node to its K next and previous neighbors. This constructs
-    % % indices for a ring lattice.
-%     N = nAg;
-%     K = 6;
-%     beta = 0.1;
-% 
-%     s = repelem((1:N)',1,K);
-%     t = s + repmat(1:K,N,1);
-%     t = mod(t-1,N)+1;
-% 
-%     % Rewire the target node of each edge with probability beta
-%     for source=1:N    
-%         switchEdge = rand(K, 1) < beta;
-% 
-%         newTargets = rand(N, 1);
-%         newTargets(source) = 0;
-%         newTargets(s(t==source)) = 0;
-%         newTargets(t(source, ~switchEdge)) = 0;
-% 
-%         [~, ind] = sort(newTargets, 'descend');
-%         t(source, switchEdge) = ind(1:nnz(switchEdge));
-%     end
-%     WS = graph(s,t);
-%     socConn = adjacency(WS);
-
-    % % ---------------
-
-
-    %Visualize network:
-    G=graph(socConn, 'OmitSelfLoops'); %create and show the graph
-    subplot(131);
-    gr=plot(G, 'Layout','force');
-    % labelnode(gr,1:nAg,1:nAg);
-    ndDeg=sum(socConn);
-
-    if(false) %show degree scaling
-    subplot(212); [num, deg] = histcounts(ndDeg); deg=(deg(2:end)+deg(1:end-1))/2;
-    cumnum=cumsum(num,'reverse'); 
-    % loglog(deg,num,'.'); return
-    loglog(deg,cumnum,'.'); 
-    tmp1=polyfit(log(deg),log(cumnum),1); title(['slope=', num2str(round(tmp1(1),2))],'FontSize',12);
-    return
-    end
-    %% Initialize variables and run the dynamics
-    agSts=zeros(nAg,nId);
-    for ia=1:nAg %initialize knowledge states randomly
-      agSts(ia,randsample(nId,maxId,false))=1;
-    end
-    % agSts(1:700,:)=repmat(agSts(1,:),[700,1]);
-    % agSts(451:end,:)=repmat(agSts(end,:),[450,1]);
-    % rng(4);
-    % cols=zeros(nAg, 1);
-    agList=1:nAg;
-    % colorbar; colormap('jet');
-    % % caxis([0,2^nId]); %caxis([0,1]);
-    tic
-    ancPts=0; idEmb=2*maxId*randn(ancPts+nAg,2); %initialize TSNE
-    anc=repmat(agSts(1,:),[ancPts,1]);
-    if(saveMov); 
-      writerObj = VideoWriter('out.mp4','MPEG-4'); % Name it.
-    % writerObj.FrameRate = 40; % How many frames per second.
-    open(writerObj);
-    end
-    t_count = 1;
-    for it=1:tSteps
-      %% The algorithm at each time-step-----------------------
-      ia=randi(nAg); %choose agent to update %mod(it,nAg)+1;%
-      nghbrs=agList(logical(socConn(ia,:))); in=nghbrs(randi(length(nghbrs))); %choose neighbor
-    %   in=randsample(nAg,1,true,full(socConn(ia,:))); %slower
-
-    %   pInt=(agSts(ia,:)*agSts(in,:)')./maxId; %interaction probability
-    %   if(agreeFl || pInt>=0.3)%rand) %update state
-        diff=agSts(in,:)-agSts(ia,:); % =0 where same, =1 where nghb likes, =-1 where I like
-        if(sum(abs(diff))>0) %if there are any differences
-    %     tmp=find(agSts(ia,:));%find(diff<0); 
-        tmp=find(diff<0); agSts(ia,tmp(randi(length(tmp))))=0; %remove old idea
-    %     agSts(ia,randsample(nId,1,true,diff<0))=0; %slow
-        tmp=find(diff>0); agSts(ia,tmp(randi(length(tmp))))=1; %add new idea
-        end
-
-      %% Analyzing and showing the two networks-------------------
-    %   cols(ia) = mean(agSts(ia, :)*agSts(nghbrs, :)')./maxId; 
-      if(mod(it,1E3)==1 && it>-7E5)
-    %     cols=sum(socConn.*(agSts*agSts'))./maxId./ndDeg; %neighbor similarity metric
-        cols=bi2de(agSts);
-        gr.NodeCData=cols; [uCol,uix]=unique(cols);
-        subplot(131); title(['time: ',num2str(it)]);%,';   ',num2str(length(uCol)),' unique cultures']);
-
-        idConn=agSts'*agSts; idConn=idConn-diag(diag(idConn));
-    %     idConn(idConn<max(max(idConn))/10)=0; %cut off weak links for visual clarity
-        idConn_data(:,:,t_count) = idConn; %3D array stores states of idConn array at sequential time steps
-        idConn_total_data(:,:,t_count,rng_seed) = idConn; %4D array stores states of idConn array at sequential times steps with 4th dim being interation of simulation
-
-        glob_eff(t_count) = efficiency_bin(idConn);
-        t_count = t_count + 1;
-
-      end
-    end
-    toc
-
-    % plot(glob_eff)
-
-
-    % save(['idConn_data_BA_rng' num2str(rng_seed) '_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '.mat'], 'idConn_data')
-    % save(['glob_eff_BA_rng' num2str(rng_seed) '_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '.mat'], 'glob_eff')
-    % glob_eff_plot_data = glob_eff;
-    glob_eff_plot_data(rng_seed, :) = glob_eff;
-end
-save(['idConn_data_BA_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_data')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-for rng_seed=1:rng_max
-    %% Construct the network
-    ndDeg=zeros(1,nAg)+1E-3;
-%     rng_seed = 4;
-    rng(rng_seed);
-
-    % % ----1---- BA (preferential attachment)
-%     for ii=1:nAg
-%       for ir=1:20; rand; end %ensure rand is uncorrelated
-%       for ie=1
-%         if(ie>1  && rand>1.01); continue; end %modulate connectivity
-%       attTo=randsample(nAg,1,true,ndDeg.^1); %preferntial attachment
-%       socConn(ii,attTo)=1; socConn(attTo,ii)=1;
-%       ndDeg(ii)=round(ndDeg(ii)+1); ndDeg(attTo)=round(ndDeg(attTo)+1);
-%       end
-%     end
-
-    % ----2---- Square Lattice
+  case 2
+%     ----2---- Square Lattice
     socConn=delsq(numgrid('S', nAgSqrt+2)); socConn=-socConn+diag(diag(socConn));
-
-    % % ----3---- WS
-    % % Watts-Strogatz model graph with N
-    % % nodes, N*K edges, mean node degree 2*K, and rewiring probability beta.
-    % %
-    % % beta = 0 is a ring lattice, and beta = 1 is a random graph.
-    % 
-    % % Connect each node to its K next and previous neighbors. This constructs
-    % % indices for a ring lattice.
-%     N = nAg;
-%     K = 6;
-%     beta = 0.1;
-% 
-%     s = repelem((1:N)',1,K);
-%     t = s + repmat(1:K,N,1);
-%     t = mod(t-1,N)+1;
-% 
-%     % Rewire the target node of each edge with probability beta
-%     for source=1:N    
-%         switchEdge = rand(K, 1) < beta;
-% 
-%         newTargets = rand(N, 1);
-%         newTargets(source) = 0;
-%         newTargets(s(t==source)) = 0;
-%         newTargets(t(source, ~switchEdge)) = 0;
-% 
-%         [~, ind] = sort(newTargets, 'descend');
-%         t(source, switchEdge) = ind(1:nnz(switchEdge));
-%     end
-%     WS = graph(s,t);
-%     socConn = adjacency(WS);
-
-    % % ---------------
-
-
-    %Visualize network:
-    G=graph(socConn, 'OmitSelfLoops'); %create and show the graph
-    subplot(131);
-    gr=plot(G, 'Layout','force');
-    % labelnode(gr,1:nAg,1:nAg);
-    ndDeg=sum(socConn);
-
-    if(false) %show degree scaling
-    subplot(212); [num, deg] = histcounts(ndDeg); deg=(deg(2:end)+deg(1:end-1))/2;
-    cumnum=cumsum(num,'reverse'); 
-    % loglog(deg,num,'.'); return
-    loglog(deg,cumnum,'.'); 
-    tmp1=polyfit(log(deg),log(cumnum),1); title(['slope=', num2str(round(tmp1(1),2))],'FontSize',12);
-    return
-    end
-    %% Initialize variables and run the dynamics
-    agSts=zeros(nAg,nId);
-    for ia=1:nAg %initialize knowledge states randomly
-      agSts(ia,randsample(nId,maxId,false))=1;
-    end
-    % agSts(1:700,:)=repmat(agSts(1,:),[700,1]);
-    % agSts(451:end,:)=repmat(agSts(end,:),[450,1]);
-    % rng(4);
-    % cols=zeros(nAg, 1);
-    agList=1:nAg;
-    % colorbar; colormap('jet');
-    % % caxis([0,2^nId]); %caxis([0,1]);
-    tic
-    ancPts=0; idEmb=2*maxId*randn(ancPts+nAg,2); %initialize TSNE
-    anc=repmat(agSts(1,:),[ancPts,1]);
-    if(saveMov); 
-      writerObj = VideoWriter('out.mp4','MPEG-4'); % Name it.
-    % writerObj.FrameRate = 40; % How many frames per second.
-    open(writerObj);
-    end
-    t_count = 1;
-    for it=1:tSteps
-      %% The algorithm at each time-step-----------------------
-      ia=randi(nAg); %choose agent to update %mod(it,nAg)+1;%
-      nghbrs=agList(logical(socConn(ia,:))); in=nghbrs(randi(length(nghbrs))); %choose neighbor
-    %   in=randsample(nAg,1,true,full(socConn(ia,:))); %slower
-
-    %   pInt=(agSts(ia,:)*agSts(in,:)')./maxId; %interaction probability
-    %   if(agreeFl || pInt>=0.3)%rand) %update state
-        diff=agSts(in,:)-agSts(ia,:); % =0 where same, =1 where nghb likes, =-1 where I like
-        if(sum(abs(diff))>0) %if there are any differences
-    %     tmp=find(agSts(ia,:));%find(diff<0); 
-        tmp=find(diff<0); agSts(ia,tmp(randi(length(tmp))))=0; %remove old idea
-    %     agSts(ia,randsample(nId,1,true,diff<0))=0; %slow
-        tmp=find(diff>0); agSts(ia,tmp(randi(length(tmp))))=1; %add new idea
-        end
-
-      %% Analyzing and showing the two networks-------------------
-    %   cols(ia) = mean(agSts(ia, :)*agSts(nghbrs, :)')./maxId; 
-      if(mod(it,1E3)==1 && it>-7E5)
-    %     cols=sum(socConn.*(agSts*agSts'))./maxId./ndDeg; %neighbor similarity metric
-        cols=bi2de(agSts);
-        gr.NodeCData=cols; [uCol,uix]=unique(cols);
-        subplot(131); title(['time: ',num2str(it)]);%,';   ',num2str(length(uCol)),' unique cultures']);
-
-        idConn=agSts'*agSts; idConn=idConn-diag(diag(idConn));
-    %     idConn(idConn<max(max(idConn))/10)=0; %cut off weak links for visual clarity
-        idConn_data(:,:,t_count) = idConn; %3D array stores states of idConn array at sequential time steps
-        idConn_total_data(:,:,t_count,(rng_max + rng_seed)) = idConn; %4D array stores states of idConn array at sequential times steps with 4th dim being interation of simulation
-
-        glob_eff(t_count) = efficiency_bin(idConn);
-        t_count = t_count + 1;
-
-      end
-    end
-    toc
-
-    % plot(glob_eff)
-
-
-    % save(['idConn_data_BA_rng' num2str(rng_seed) '_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '.mat'], 'idConn_data')
-    % save(['glob_eff_BA_rng' num2str(rng_seed) '_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '.mat'], 'glob_eff')
-    % glob_eff_plot_data = glob_eff;
-    glob_eff_plot_data(rng_max + rng_seed, :) = glob_eff;
-end
-save(['idConn_data_lattice_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_data')
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-for rng_seed=1:rng_max
-    %% Construct the network
-    ndDeg=zeros(1,nAg)+1E-3;
-%     rng_seed = 4;
-    rng(rng_seed);
-
-    % % ----1---- BA (preferential attachment)
-%     for ii=1:nAg
-%       for ir=1:20; rand; end %ensure rand is uncorrelated
-%       for ie=1
-%         if(ie>1  && rand>1.01); continue; end %modulate connectivity
-%       attTo=randsample(nAg,1,true,ndDeg.^1); %preferntial attachment
-%       socConn(ii,attTo)=1; socConn(attTo,ii)=1;
-%       ndDeg(ii)=round(ndDeg(ii)+1); ndDeg(attTo)=round(ndDeg(attTo)+1);
-%       end
-%     end
-
-    % ----2---- Square Lattice
-%     socConn=delsq(numgrid('S', nAgSqrt+2)); socConn=-socConn+diag(diag(socConn));
-
-    % % ----3---- WS
-    % % Watts-Strogatz model graph with N
-    % % nodes, N*K edges, mean node degree 2*K, and rewiring probability beta.
-    % %
-    % % beta = 0 is a ring lattice, and beta = 1 is a random graph.
-    % 
-    % % Connect each node to its K next and previous neighbors. This constructs
-    % % indices for a ring lattice.
+  case 3
+    % ----3---- WS
+    % Watts-Strogatz model graph with N
+    % nodes, N*K edges, mean node degree 2*K, and rewiring probability beta.
+    %
+    % beta = 0 is a ring lattice, and beta = 1 is a random graph.
+    
+    % Connect each node to its K next and previous neighbors. This constructs
+    % indices for a ring lattice.
     N = nAg;
     K = 6;
     beta = 0.1;
@@ -352,12 +80,12 @@ for rng_seed=1:rng_max
     WS = graph(s,t);
     socConn = adjacency(WS);
 
-    % % ---------------
-
+    % ---------------
+end
 
     %Visualize network:
     G=graph(socConn, 'OmitSelfLoops'); %create and show the graph
-    subplot(131);
+    subplot(121);
     gr=plot(G, 'Layout','force');
     % labelnode(gr,1:nAg,1:nAg);
     ndDeg=sum(socConn);
@@ -409,21 +137,43 @@ for rng_seed=1:rng_max
 
       %% Analyzing and showing the two networks-------------------
     %   cols(ia) = mean(agSts(ia, :)*agSts(nghbrs, :)')./maxId; 
-      if(mod(it,1E3)==1 && it>-7E5)
-    %     cols=sum(socConn.*(agSts*agSts'))./maxId./ndDeg; %neighbor similarity metric
-        cols=bi2de(agSts);
-        gr.NodeCData=cols; [uCol,uix]=unique(cols);
-        subplot(131); title(['time: ',num2str(it)]);%,';   ',num2str(length(uCol)),' unique cultures']);
-
-        idConn=agSts'*agSts; idConn=idConn-diag(diag(idConn));
-    %     idConn(idConn<max(max(idConn))/10)=0; %cut off weak links for visual clarity
-        idConn_data(:,:,t_count) = idConn; %3D array stores states of idConn array at sequential time steps
-        idConn_total_data(:,:,t_count,((2 * rng_max) + rng_seed)) = idConn; %4D array stores states of idConn array at sequential times steps with 4th dim being interation of simulation
-
-        glob_eff(t_count) = efficiency_bin(idConn);
+   if(mod(it,1E3)==1 && it>-7E5) %execute every "this many" time-steps    
+    idConn=agSts'*agSts; idConn=idConn-diag(diag(idConn)); %compute weights of the semantic net
+%     idConn(idConn<max(max(idConn))/10)=0; %cut off weak links for visual clarity
+%     idConn_data(:,:,t_count) = idConn; %3D array stores states of idConn array at sequential time steps
+    idConnAll(:,:,t_count,rng_seed+rng_max*(net_type-1)) = idConn; %4D array stores states of idConn array at sequential times steps with 4th dim being interation of simulation
+%         glob_eff(t_count) = efficiency_bin(idConn);
         t_count = t_count + 1;
+    if(mod(it,1E4)==1)   %show plots for monitoring
+%     cols=sum(socConn.*(agSts*agSts'))./maxId./ndDeg; %neighbor similarity metric
+    cols=bi2de(agSts); %color-code the unique cultures
+    gr.NodeCData=cols; [uCol,uix]=unique(cols);
+    subplot(121); title(['time: ',num2str(it)]);%,';   ',num2str(length(uCol)),' unique cultures']);
 
-      end
+    Gi=graph(idConn,'OmitSelfLoops');
+    EdWt=(Gi.Edges.Weight); LWidths = 3.5*(EdWt-min(EdWt)+1E-3)/(max(EdWt)-min(EdWt)); LWidths(LWidths==Inf)=1; %for if all weights are the same
+    if(~exist('xx','var')); xx=7*rand(nId,1); yy=7*rand(nId,1); end
+    subplot(122); giPl=plot(Gi, 'Layout','force','LineWidth',LWidths,'XStart',xx,'YStart',yy,'Iterations',1);
+    tmp=find(sum(idConn)>0); %idConn=idConn(tmp,tmp);
+    xx=get(giPl,'XData'); yy=get(giPl,'YData'); axis([min(xx(tmp)),max(xx(tmp)),min(yy(tmp)),max(yy(tmp))]);
+    title([num2str(length(tmp)),' ideas present']);
+    
+%     idConn1=agSts(uix,:); idConn1=squeeze(sum(idConn1 & permute(idConn1,[3,2,1]),2));
+%     Gi=graph(idConn1,'OmitSelfLoops');
+%     EdWt=(Gi.Edges.Weight); LWidths = 5*(EdWt-min(EdWt)+1E-3)/(max(EdWt)-min(EdWt)); LWidths(LWidths==Inf)=1; %for if all weights are the same
+%     subplot(212); giPl=plot(Gi, 'Layout','force','LineWidth',LWidths,'LineStyle','none');
+    
+%     %embed all present ideas in dim-reduced space
+%     idEmb=tsne([anc;agSts(:,:)],'Distance','cityblock','InitialY',idEmb,...
+%       'Options',statset('MaxIter',50)); 
+%     subplot(133); cla; scatter(idEmb(ancPts+1:end,1),idEmb(ancPts+1:end,2),3,cols); axis([-1,1,-1,1]*maxId*1.3);
+%     hold on; plot(idEmb(1:ancPts,1),idEmb(1:ancPts,2),'*r'); %show ancor points
+%     title([num2str(length(uCol)),' unique cultures']);
+    drawnow;
+    if(saveMov); writeVideo(writerObj, getframe(gcf)); end
+    end
+  end
+      
     end
     toc
 
@@ -433,15 +183,17 @@ for rng_seed=1:rng_max
     % save(['idConn_data_BA_rng' num2str(rng_seed) '_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '.mat'], 'idConn_data')
     % save(['glob_eff_BA_rng' num2str(rng_seed) '_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '.mat'], 'glob_eff')
     % glob_eff_plot_data = glob_eff;
-    glob_eff_plot_data((2 * rng_max) + rng_seed, :) = glob_eff;
+%     glob_eff_plot_data(rng_seed, :) = glob_eff;
 end
+end
+% save(['idConn_data_BA_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_data')
 
-save(['idConn_data_WS_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_data')
+% save(['idConn_data_WS_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_data')
 
-save(['idConn_total_data_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_total_data')
+% save(['idConn_total_data_nAg' num2str(nAg) '_nID' num2str(nId) '_maxId' num2str(maxId) '_tSteps' num2str(tSteps) '_rng_max' num2str(rng_max) '_' datestr(now, 'YYYY-mm-dd_HH:MM:SS:FFF') '.mat'], 'idConn_total_data')
 
-
-
+return;
+%%
 plot(glob_eff_plot_data(1, :))
 hold on
 for plotn=1:rng_max
